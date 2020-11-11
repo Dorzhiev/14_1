0757fe00a5f3779e9de215b4ea6590a5f46079c2
Ayur Dorzhiev <ayurra@mail.ru>
2020-11-11 03:11:05 +0300

    14_1

--- script.js
+++ script.js
@@ -6,24 +6,42 @@ function DomElement(selector, height, width, bg, fontSize){
     this.fontSize = fontSize;
 };
 
-DomElement.prototype.createsElements = function(){
+DomElement.prototype.createElements = function(){
+    let block;
+    console.log(this.selector[0]);
     if(this.selector[0]=='.'){
        console.log('class="block"');
+       block = document.createElement('div');
+             block.textcontent='hello';
+      
+        block.style.cssText = `
+            height: ${this.height}; 
+            width: ${this.width}; 
+            background: ${this.bg}; 
+            font-size: ${this.fontSize};
+            `;
+        document.body.append(block);
     };
+    
     if(this.selector[0]=='#'){
-       console.log('id ="best"');
+      block = document.createElement('p');
+      block.textcontent='hello';
+      
+    block.style.cssText = `
+        height: ${this.height}; 
+        width: ${this.width}; 
+        background: ${this.bg}; 
+        font-size: ${this.fontSize};
+        `;
+      document.body.append(block);
     };
     
 };
 
-DomElement2.style.cssText = `
-    height: ${this.height}; 
-    width: ${this.width}; 
-    background: ${this.bg}; 
-    font-size: ${this.fontSize};
-    `;
 
 
-let DomElement2 = new DomElement('.block','35px','100px','#eee','15px');
-DomElement2.createsElements();
+let DomElement2 = new DomElement('#block','35px','100px','#eee','15px');
+DomElement2.createElements();
 
+let DomElement3 = new DomElement('.block','65px','50px','green','14px');
+DomElement2.createElements();
